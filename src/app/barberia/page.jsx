77ba805c51d7e4d6b0482634d01342aeb6caa382
sgaray512 <s.garay@itecriocuarto.org.ar>
2026-04-16 22:36:42 -0300

    correccion de identacion

--- src/app/barberia/page.jsx
+++ src/app/barberia/page.jsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function Barberia() {
   return (
     <div>
-       <h2 className="text-2xl font-bold mb-4">
-          Sección Barbería:
-        </h2>
-        <div className="bg-gray-900 text-white p-4 flex gap-4">
+      <h2 className="text-2xl font-bold mb-4">
+        Sección Barbería:
+      </h2>
+      <div className="bg-gray-900 text-white p-4 flex gap-4">
     
-          <Link href="/barberia/productos">Ver Nuestros Productos</Link>
-        </div>
+        <Link href="/barberia/productos">Ver Nuestros Productos</Link>
+      </div>
     </div>
   );
 }
